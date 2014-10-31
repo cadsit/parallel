26e6a4912584b7de3a942038564cde900bbd466d
Connor Adsit <cda8519@cs.rit.edu>
2014-10-31 10:53:33 -0400

    Added in initialization and some helper functions

--- WalkSATSeq.java
+++ WalkSATSeq.java
@@ -7,9 +7,9 @@
  */
 
 import edu.rit.pj2.Task;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.BufferedReader;
+import java.util.Scanner;
 import edu.rit.util.Random;
 
 /**
@@ -19,27 +19,39 @@ import edu.rit.util.Random;
  *        N - number of iterations to perform
  *        seed - long seed for PRNG
  *        inputFile.cnf - filepath for input, in CNF form
+ * 
+ * Input file format:
+ *    first line: V C
+ *       V - total number of variables
+ *       C - total number of clauses
+ *    next C lines:
+ *       variable numbers delimited by a space, lines ending in 0
  *
  * @author Connor Adsit
  * @author Kevin Bradley
  * @author Christian Heinrich
  */
 public class WalkSATSeq extends edu.rit.pj2.Task {
-
-   boolean[] truth;
-   int[][] clauses;
+   int numVars;
+   int numClauses;
+   int[] truth;
+   int[][] equation;
+   boolean[] clauses;
    long seed;
    long maxIter;
-   BufferedReader bf;
+   Scanner sc;
+   Random prng;
+   boolean satisfied;
 
    /**
-    * Main Function
+    * Main Function:
     * @param   args  contains the number of iterations to perform
     *                and the path to the input file
     */
    public void main (String[] args) {
       try {
-         // read in input
+         // parse command line args
+         
          if (args.length != 3)
             usage();
          maxIter = Long.parseLong(args[0]);
@@ -47,7 +59,39 @@ public class WalkSATSeq extends edu.rit.pj2.Task {
             throw new NumberFormatException();
          }
          seed = Long.parseLong(args[1]);
-         bf = new BufferedReader(new FileReader (args[2]));
+         sc = new Scanner(new FileInputStream (args[2]));
+
+         // read in input
+         sc.nextLine();
+         String[] line = sc.nextLine().split(" ");
+         numVars = Integer.parseInt(line[2]);
+         numClauses = Integer.parseInt(line[3]);
+         equation = new int[numClauses][];
+         for (int i = 0; i < numClauses; ++i) {
+            line = sc.nextLine().split(" ");
+            int clauseLength = line.length - 1;
+            equation[i] = new int[clauseLength];
+            for (int j = 0; j < clauseLength; ++j) {
+               equation[i][j] = Integer.parseInt(line[j]);
+            }
+         }
+         truth = new int[(numVars + 1) / 2];
+         clauses = new boolean[numClauses];
+         satisfied = false;
+         prng = new Random(seed);
+
+         // generate initial assignment
+         for (int i = 0; i < truth.length; ++i) {
+            truth[i] = prng.nextInteger();
+         }
+         update();
+
+         // perform local search
+         for (long l = 0L; l < maxIter; ++l) {
+            if (satisfied)
+               break;
+            
+         }
       } catch (NumberFormatException nfe) {
          usage();
       } catch (FileNotFoundException fnfe) {
@@ -57,11 +101,14 @@ public class WalkSATSeq extends edu.rit.pj2.Task {
          System.err.println(e.getMessage());
       } finally {
          try {
-            bf.close();
+            sc.close();
          } catch (Exception e) {}
       }
    }
 
+   /**
+    * Prints out a usage statement
+    */
    private void usage() {
       System.err.println("Usage: java pj2 WalkSATSeq <N> <seed> <file>");
       System.err.println("N - long number of iterations");
@@ -69,4 +116,29 @@ public class WalkSATSeq extends edu.rit.pj2.Task {
       System.err.println("file - input file of CNF equation");
       System.exit(0);
    }
+
+   private void update() {
+      boolean allTrue = true;
+      for (int i = 0; i < numClauses; ++i) {
+         boolean marker = true;
+         for (int x : equation[i]) {
+            if (x < 0) {
+               marker &= !lookup(-(x + 1));
+            } else {
+               marker &= lookup (x - 1);
+            }
+         }
+         clauses[i] = marker;
+         if (!marker)
+            allTrue = false;
+      }
+      satisfied = allTrue;
+   }
+
+   private boolean lookup(int index) {
+      int i = index / 32;
+      int assignment = truth[i] & (1 << (index % 32));
+      return assignment == 1;
+   }
+
 }
